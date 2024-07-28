c1991afbaffefdfeb27f867ea5689d1e2bbf5779
raioramalho <ramalho.sit@gmail.com>
2024-07-28 13:24:58 -0300

    version 1.1.5-b

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@
  * Este projeto contém funções, classes e interfaces para ajudar no dia a dia.
  * 
  * @author Alan Ramalho
- * @version 1.0
+ * @version 1.1.5-b
  * @date 26-07-2024
  */
 
@@ -15,7 +15,19 @@
 import { CepService } from "./services/cep/cep.service";
 
 /**
+ * Importação do serviço de Imagem
+ * Este serviço é responsável por operações relacionadas a Imagem
+ */
+import { ImageService } from "./services/image/image.service";
+
+/**
  * Exportação do serviço de CEP
  * Permite que outros módulos utilizem o CepService
  */
 export { CepService };
+
+/**
+ * Exportação do serviço de Imagem
+ * Permite que outros módulos utilizem o ImageService
+ */
+export { ImageService };
